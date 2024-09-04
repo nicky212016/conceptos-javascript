c7d9b2fd7b475794975b33017207ca4565ad4f9b
NICOLAS CORTES RESTREPO <nicolas.cortes@uao.edu.co>
2024-09-03 19:26:05 -0500

    Variables, arrays y objetos

--- basics.js
+++ basics.js
@@ -1,1 +1,31 @@
 console.log("Ola")
+
+//Variables
+//let - Es una variable que puede cambiar su valor
+let nombreDeLaVariable = "valor de la variable"
+//const - Es una variable que no puede cambiar su valor, pues es un constante
+const variableQueNoQuieroCambiar = "siuuuuu"
+
+//tipos de datos
+let variable =  "string";
+console.log(variable);
+let numeroEdad = 20;
+console.log(numeroEdad);
+let variableNula = null; //variable nula asignada por nosotros
+let variableNoDefinida = undefined; //variable no definida asignada por el sistema
+let variableSimbolo = Symbol(); //valor que no cambia en la ejecucion del programa
+
+//Estructuras de datos
+//Arrays
+let marcas = ["nike", "adidas", "puma"];
+console.log(marcas);
+console.log(marcas.length);
+
+//Objetos
+let perro = {
+    nombre: 'Gordo',
+    edad: '10',
+    raza: 'Schnauzer'
+}
+console.log(perro);
+console.log(perro.nombre);
